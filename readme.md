1aafca118a2f679db82131885decf63fdaf28029
Joshua Moyers <jmoyers@gmail.com>
2012-09-23 20:29:02 -0700

    initial multi file support

--- readme.md
+++ readme.md
@@ -1,16 +1,66 @@
-<a name="top" />
-
-#Docs
-
-* [Parser](#Parser)
+* [Parser2](#Parser2) (test2.js)
+    * [readFile(String file)](#Parser2-readFile) - Helper method for reading a file into the log Parser2 directly. 
+    * [write(String line)](#Parser2-write) - Write log file lines to be parsed
+    * [parseTransportData(String line)](#Parser2-parseTransportData) - Parse sococo transport data out of a log line
+* [Parser](#Parser) (test.js)
     * [readFile(String file)](#Parser-readFile) - Helper method for reading a file into the log parser directly. 
     * [write(String line)](#Parser-write) - Write log file lines to be parsed
     * [parseTransportData(String line)](#Parser-parseTransportData) - Parse sococo transport data out of a log line
 
+
+<a name="Parser2" />
+##Parser2 &bull; [top](#top)
+
+Location: test2.js
+
+A class for parsing sococo log files and emitting structured data
+
+This conforms to the node Stream base class, and can be chained together with other stream filters
+using the `.pipe()` method. This class expects data to be in line format rather than chunks of
+data coming directly from the file. You can use readFile to pass in a file directly, and it will
+convert into buffered lines for you.
+    
+    var p = new Parser22();
+    p.on('data', function(data){
+        console.log(data);
+    });
+    p.on('end', function(){
+        console.log('parsing complete');
+    });
+    p.readFile('test-log.txt');
+
+
+
+<a name="Parser2-readFile">
+###[Parser2](#Parser2)#readFile(String file)
+
+Helper method for reading a file into the log Parser2 directly. 
+
+Assumes first line of the file is going to be a header, so skips it for parsing. Buffers input 
+until newline, then feeds each line into the Parser2. When its finished, it emits an end event. 
+Because parsing is synchronous, you can depend on parsing being complete.
+
+
+<a name="Parser2-write">
+###[Parser2](#Parser2)#write(String line)
+
+Write log file lines to be parsed
+
+Conforming to the stream api, mostly so you can parse together data transform streams. This stream
+expects lines in string format.
+
+
+<a name="Parser2-parseTransportData">
+###[Parser2](#Parser2)#parseTransportData(String line)
+
+Parse sococo transport data out of a log line
+
+We have a header on log messages that conforms roughly to a {{name:value;name:value}} format. This
+method parses that data off raw log message and returns a javascript object.
 <a name="Parser" />
 ##Parser &bull; [top](#top)
 
-
+Location: test.js
 
 A class for parsing sococo log files and emitting structured data
 
---
+++ test2.js
@@ -0,0 +1,148 @@
+var fs = require('fs'),
+  util = require('util'),
+  lazy = require('lazy'),
+  Stream = require('stream').Stream;
+
+/**
+* A class for parsing sococo log files and emitting structured data
+* 
+* This conforms to the node Stream base class, and can be chained together with other stream filters
+* using the `.pipe()` method. This class expects data to be in line format rather than chunks of
+* data coming directly from the file. You can use readFile to pass in a file directly, and it will
+* convert into buffered lines for you.
+*     
+*     var p = new Parser22();
+*     p.on('data', function(data){
+*         console.log(data);
+*     });
+*     p.on('end', function(){
+*         console.log('parsing complete');
+*     });
+*     p.readFile('test-log.txt');
+*/
+function Parser2(){
+  Stream.call(this);
+}
+
+util.inherits(Parser2, Stream);
+
+/**
+* Helper method for reading a file into the log Parser2 directly. 
+* 
+* Assumes first line of the file is going to be a header, so skips it for parsing. Buffers input 
+* until newline, then feeds each line into the Parser2. When its finished, it emits an end event. 
+* Because parsing is synchronous, you can depend on parsing being complete.
+*
+* @param {String} file name of file to be read in
+* @api public
+*/
+Parser2.prototype.readFile = function(file) {
+  var that = this;
+
+  new lazy(fs.createReadStream(file))
+    .lines
+    .skip(1)
+    .forEach(function(line){
+      that.write(line.toString());
+    })
+    .join(function(){
+      that.emit('end');
+    });
+}
+
+/**
+* Write log file lines to be parsed
+* 
+* Conforming to the stream api, mostly so you can parse together data transform streams. This stream
+* expects lines in string format.
+*
+* @param {String} line one line of a sococo log
+* @api private
+*/
+Parser2.prototype.write = function(line) {
+  var that = this,
+    items = line.split(',');
+
+  if (items.length < 8) {
+    return;
+  }
+
+  var mess = items[8].replace(/\n/g, '').replace(/\r/g, '');
+
+  if (items.length > 9) {
+    for (var j = 9; j < items.length; j++) {
+      mess += ',' + items[j];
+    }
+  }
+
+  if (mess[0] == ' '){
+    mess = mess.substring(1);
+  }
+
+  var transportData = this.parseTransportData(mess);
+  mess = !~mess.indexOf('}}') ? mess : mess.split('}}')[1].replace(/^\s+/,"");
+
+  var entry = {
+    cpuId: items[0].replace(/ /g, ''),
+    processId: items[1].replace(/ /g, ''),
+    threadId: items[2].replace(/ /g, ''),
+    diffTime: items[3].replace(/ /g, ''),
+    systemDate: items[4].replace(/ /g, ''),
+    systemTime: items[5].replace(/ /g, ''),
+    severity: items[6].replace(/ /g, ''),
+    facility: items[7].replace(/ /g, ''),
+    transportData: transportData,
+    message: mess
+  };
+
+  this.emit('data', entry);
+}
+
+/**
+* Parse sococo transport data out of a log line
+* 
+* We have a header on log messages that conforms roughly to a {{name:value;name:value}} format. This
+* method parses that data off raw log message and returns a javascript object.
+*
+* @param {String} line one line of a sococo log
+* @api private
+*/
+Parser2.prototype.parseTransportData = function(line) {
+  if (!~line.indexOf('{{')) {
+    return false;
+  }
+
+  var data = line.split('}}')[0].split('{{')[1];
+
+  data = data.split(';');
+
+  var transportData = {}
+
+  if (data[0] == 'r') {
+    transportData['direction'] = 'receiving';
+  } else if (data[0] == 's') {
+    transportData['direction'] = 'sending';
+  }
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].indexOf('#=') != -1) {
+      transportData['packetNumber'] = data[i].split('=')[1];
+    }
+    if (data[i].indexOf('s=') != -1) {
+      transportData['session'] = data[i].split('=')[1];
+    }
+    if (data[i].indexOf('c=') != -1) {
+      transportData['channel'] = data[i].split('=')[1];
+    }
+    if (data[i].indexOf('t=') != -1) {
+      transportData['payloadType'] = data[i].split('=')[1];
+    }
+    if (data[i].indexOf('m=') != -1) {
+      transportData['mediaElementId'] = data[i].split('=')[1];
+    }
+  }
+
+  return transportData;
+}
+
+module.exports = Parser2;
